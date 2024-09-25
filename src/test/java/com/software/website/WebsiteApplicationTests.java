4712c57435260eee904b33f706a0c37e89c5d316
Berat <berat-murseli@hotmail.com>
2024-09-25 18:17:26 +0200

    sprinboot lagt til

---
+++ src/test/java/com/software/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
