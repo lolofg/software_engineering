c66beb6a4559c3132a78ad9c0408f17d7f2ca02f
Fifi987 <filipos123.91@gmail.com>
2024-11-14 20:12:18 +0100

    dasdasda

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "../components/Header";
+import HeaderStartPage from '../components/HeaderStartPage';
 
 export default function MainPage() {
     return(
         <>
-            <Header/>
-           <></>
+            <HeaderStartPage/>
+           <><p>Hei</p></>
         </>
     )
 }
