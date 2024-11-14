105b63020c865dbe647f59e831cbbd878ec5218b
LiGhTmITnIk <47636135+LiGhTmITnIk@users.noreply.github.com>
2024-11-14 19:44:21 +0100

    la til route for mainpageadmin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import MainPage from "./pages/MainPage";
+import MainPageAdmin from "./pages/MainPageAdmin";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<MainPage/>}/>
                 <Route path="/Login" element={<Login/>}/>
                 <Route path="/User/:UserID" element={<MainPage/>}/>
+                <Route path="/Admin" element={<MainPageAdmin/>}/>
             </Routes>
         </BrowserRouter>
     </div>
