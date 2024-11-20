bf3f357eae6edb71bc4d5159552a5713d530295c
arinpenahi <arinp@hiof.no>
2024-11-20 15:34:51 +0100

    fikset opp mainpage + opp i css

--- frontend/src/pages/MainPageUser.js
+++ frontend/src/pages/MainPageUser.js
@@ -27,6 +27,7 @@ export default function MainPageUser() {
         <>
             <Header />
             <div className="mainpage_wrapper">
+                {/* Grid med enheter */}
                 <div className="device_grid">
                     {devices.map((device, index) => (
                         <div key={index} className="device_item" onClick={() => handleDeviceClick(device)}>
@@ -35,18 +36,21 @@ export default function MainPageUser() {
                         </div>
                     ))}
                 </div>
+
+                {/* Knappene rett under grid */}
                 <div className="action_buttons">
                     <button className="button_add">Legg til enhet</button>
                     <button className="button_remove">Fjern enhet</button>
                 </div>
 
-                {/* Popup visning når en enhet er valgt */}
+                {/* Popup som vises når en enhet er valgt */}
                 {selectedDevice && (
                     <div className="device_popup">
                         <div className="popup_content">
                             <button className="close_button" onClick={closeDevicePopup}>×</button>
                             <div className="device_avatar large"></div>
                             <p>{selectedDevice.name}</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue mollis urna, et imperdiet sem vulputate vitae. Integer sit amet ultricies metus.</p>
                         </div>
                     </div>
                 )}
