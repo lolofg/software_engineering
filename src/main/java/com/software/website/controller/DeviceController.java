6aaf447151ad5b34166e22608f16abc0422c44ff
Sekkos03 <SekouKosiah82@gmail.com>
2024-11-20 19:22:25 +0100

    Laget add og remove funksjoner for devices i service og controller.

--- src/main/java/com/software/website/controller/DeviceController.java
+++ src/main/java/com/software/website/controller/DeviceController.java
@@ -2,12 +2,12 @@ package com.software.website.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.software.website.Entity.Device;
 import com.software.website.service.DeviceService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -28,8 +28,16 @@ public class DeviceController {
     public Device getOneDeviceById(@RequestParam String id) {
         return deviceService.getOneDeviceByID(id);
     }
-    
-    
-    
-    
+
+    @PostMapping
+    public ResponseEntity<String> addDevice(@RequestBody Device device) {
+        deviceService.addDevice(device);
+        return ResponseEntity.ok("Device added successfully.");
+    }
+
+    @DeleteMapping("Iventory/{id}")
+    public ResponseEntity<String> removeDevice(@PathVariable int id) {
+        deviceService.removeDevice(id);
+        return ResponseEntity.ok("Device removed successfully.");
+    }
 }
--- src/main/java/com/software/website/service/DeviceService.java
+++ src/main/java/com/software/website/service/DeviceService.java
@@ -23,5 +23,16 @@ public class DeviceService {
         return jdbcTemplate.queryForObject("select * from Users_devices WHERE Device_ID = ?", new DeviceRowMapper(), Device_ID); 
 
     }
+
+    public void addDevice(Device device){
+        String sql = "INSERT INTO Device (Device_ID, Product_ID, Name) VALUES (?, ?, ?)"; 
+        jdbcTemplate.update((sql), device.getDeviceID(), device.getProductID(), device.getName()); 
+
+    }
+
+    public void removeDevice(int id){
+    String sql = "DELETE FROM Device WHERE Device_ID = ?";
+    jdbcTemplate.update(sql, id); 
+    }
     
 }
